984c4e3819b5d003a550a741a624b4f20920b762
AbdulrahmanA81 <abdulrahman.abdulrazzag@mail.utoronto.ca>
2021-06-18 23:25:35 -0400

    Redesigning the UI for the webpage

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "./assets/css/blk-design-system-react.css";
+import "./assets/css/nucleo-icons.css";
+import "./assets/demo/demo.css";
+
+import Index from "./views/Index.js";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/components" render={(props) => <Index {...props} />} />
+      <Redirect from="/" to="/components" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,24 +1,56 @@
 import React, { Component } from "react";
 import { Button } from 'react-bootstrap';
 
+import IndexNavbar from "../components/Navbars/IndexNavbar.js";
+import PageHeader from "../components/PageHeader/PageHeader.js";
+import Footer from "../components/Footer/Footer.js";
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="Home" style={{
-        display: "flex",
-        alignItems: "center",
-        backgroundColor: "#b3ecff",
-        flexDirection: 'column'
-      }}>
-        <h1 style={{borderBottom: 'solid'}}>Name TBA</h1>
-        <br></br><br></br><br></br><br></br><br></br>
-        <div>
-          <h1>Trending Reccomendations</h1>
+      <>
+        <IndexNavbar />
+        <div
+          className="Home"
+          // style={{
+          //   display: "flex",
+          //   alignItems: "center",
+          //   backgroundColor: "#b3ecff",
+          //   flexDirection: "column",
+          // }}
+        >
+          <PageHeader />
+          <h1 style={{ borderBottom: "solid" }}>Name TBA</h1>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div>
+            <h1>Trending Reccomendations</h1>
+          </div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <Button onClick={() => this.props.history.push("/picker")}>
+            Click to go to Picker
+          </Button>
+          <Footer />
         </div>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        <Button onClick= {() => this.props.history.push("/picker")}>Click to go to Picker</Button>
-      </div>
+      </>
     );
   }
 }
