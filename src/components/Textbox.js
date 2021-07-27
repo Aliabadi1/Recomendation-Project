7971b3354688fe7e7f5c8c58e9f60f55c9a3fdb1
alisina abadi <alisina.abadi1@gmail.com>
2021-07-27 17:39:22 -0400

    Generation button functional

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -21,50 +21,79 @@ class Textbox extends React.Component {
         var curr;
         for(let x=0; x < this.state.shows.length;x++){
           if(this.state.arr[i] === this.state.shows[x].title.slice(1)){
+            var toadd = {};
+            var rankings = [this.state.shows[x].rank];
+            var showScores = {};
+            var max = 0;
             curr = this.state.shows[x].genres.slice(1, this.state.shows[x].genres.length - 1);
             curr = curr.split(',');
-            for(let a = 0; a < curr.length;a++){
-              if(curr[a][0] === "'"){
-                curr[a] = curr[a].slice(1, curr[a].length - 1);
+            for(let y = 0; y < curr.length; y++){
+              if(curr[y][0] === " "){
+                curr[y] = curr[y].slice(2, curr[y].length - 1);
               }else{
-                curr[a] = curr[a].slice(2, curr[a].length - 1);
+                curr[y] = curr[y].slice(1, curr[y].length - 1);
               }
             }
-            var temp = true;
-            for(let y = 0; y < this.state.shows.length; y++){
-              var recs = [];
-              var show;
-              // show = this.state.shows[y].genres.slice(1, this.state.shows[y].genres.length - 1);
-              // show = show.split(',');
-              // for(let a = 0; a < show.length;a++){
-              //   if(show[a][0] === "'"){
-              //     show[a] = show[a].slice(1, show[a].length - 1);
-              //   }else{
-              //     show[a] = show[a].slice(2, show[a].length - 1);
-              //   }
-              // }
-              // for(let z = 0; z < curr.length; z++){
-              //   if(!show.includes(curr[z])){
-              //     console.log(show, curr)
-              //     temp = false
-              //     break;
-              //   }
-              // }
-              if(this.state.shows[y].genres.indexOf(curr[0]) !== -1){
-                recs.push(this.state.shows[y]);
+            for(let y = 0; y < this.state.shows.length;y++){
+              for(let z = 0; z < curr.length;z++){
+                if(this.state.shows[y].genres.includes(curr[z])){
+                  if(toadd[this.state.shows[y].title]){
+                    toadd[this.state.shows[y].title] += 1
+                    if(toadd[this.state.shows[y].title] > max){
+                      max = toadd[this.state.shows[y].title];
+                    }
+                  }else if(!rankings.includes(this.state.shows[y].rank)){
+                    rankings.push(this.state.shows[y].rank)
+                    toadd[this.state.shows[y].title] = 1
+                    showScores[this.state.shows[y].title] = this.state.shows[y].score;
+                    if(toadd[this.state.shows[y].title] > max){
+                      max = toadd[this.state.shows[y].title];
+                    }
+                  }
+                }
               }
             }
-            console.log(recs, curr[0]);
+            var recs = {};
+            var toSort = [];
+            for(var k in toadd){
+              if(toadd[k] >= max - 2){
+                recs[k] = showScores[k];
+                toSort.push(showScores[k]);
+              }
+            }
+            var toPrint = []
+            toSort.sort().reverse();
+            for(let b = 0; b < 5; b++){
+              for(var k in recs){
+                if(toSort[b] == recs[k]){
+                  toPrint.push(k)
+                  delete recs[k];
+                }
+              }
+            }
+            var temp = [this.state.shows[x].title]
+            temp.push(toPrint)
+            var check = true;
+            for(let q = 0; q < this.state.results.length; q++){
+              if(this.state.results[q][0] === this.state.shows[x].title){
+                check = false;
+              }
+            }
+            if(check){
+              this.setState({results: [...this.state.results, temp]})
+            }
+            
+            break;
           }
-          
         }
       }
     }
+
     handleChange(e){
       this.setState({fname:e.target.value});
     }
     handleClick(e){
-        if(this.state.arr.length < 5){
+        if(this.state.arr.length < 1){
           if(!this.state.arr.includes(this.state.fname)){
             var temp = false;
             for(let x = 0; x < this.state.shows.length; x++){
@@ -105,12 +134,16 @@ class Textbox extends React.Component {
         <h5>{this.state.arr.map((goal, index) => <Alert variant='info' key={index}>{goal}</Alert>)}</h5>
         <div>
           <span>
-            <Button onClick ={() => this.setState({arr:[]})} variant="warning">Reset</Button>
+            <Button onClick ={() => this.setState({arr:[], results: []})} variant="warning">Reset</Button>
           </span>
           <span>
             <Button onClick={this.handleSubmit}>Generate Reccomendations</Button>
           </span>
         </div>
+        
+        <h5>{this.state.results.map((goal, index) => <Alert variant='info' key={index}>Since You Enjoy {goal[0]}, You Might Also Enjoy 
+        <h5>{goal[1].map((drip, inde) => <Alert variant='info' key={inde}>{drip}</Alert>)}</h5></Alert>)}</h5>
+        
       </div>
       )
     }
